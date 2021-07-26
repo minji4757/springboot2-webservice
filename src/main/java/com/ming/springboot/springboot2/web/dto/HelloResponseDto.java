aa1ca1c8d04d91d21ab34fcd91b9fbd33af8808b
minji4757 <khy1850082**>
2021-07-27 03:35:56 +0900

    add testCode

---
+++ src/main/java/com/ming/springboot/springboot2/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.ming.springboot.springboot2.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
